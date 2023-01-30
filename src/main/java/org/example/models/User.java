14526f7d7584edac09a413353665992edb6788a1
roman <romanze1706@gmail.com>
2023-01-30 14:01:36 +0300

    Class User

---
+++ src/main/java/org/example/models/User.java
@@ -0,0 +1,42 @@
+package org.example.models;
+
+import java.util.Objects;
+
+public class User {
+    private String name;
+
+    public User() {
+    }
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return name.equals(user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
